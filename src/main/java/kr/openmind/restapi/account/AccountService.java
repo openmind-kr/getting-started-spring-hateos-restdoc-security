9d10564e47d71a1ccc5fd607b28f9668551890bc
openmind-kr <slanderer@hanmail.net>
2018-11-27 02:19:23 +0900

    Fix test case - loadUser,create account

--- src/main/java/kr/openmind/restapi/account/AccountService.java
+++ src/main/java/kr/openmind/restapi/account/AccountService.java
@@ -1,18 +1,46 @@
 package kr.openmind.restapi.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Primary
 @Service
 public class AccountService implements UserDetailsService {
 
-    // FIXME
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private static final String GRANTED_AUTHORITY_PREFIX = "ROLE_";
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmailIgnoreCase(email)
+            .orElseThrow(() -> new UsernameNotFoundException(String.format("Not found email address: %s", email)));
+        return new User(email, account.getPassword(), authorities(account.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> accountRoles) {
+        return accountRoles.stream()
+            .map(accountRole -> new SimpleGrantedAuthority(GRANTED_AUTHORITY_PREFIX.concat(accountRole.name())))
+            .collect(Collectors.toSet());
+    }
+
+    public Account createAccount(Account account) {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        return accountRepository.save(account);
     }
 }
