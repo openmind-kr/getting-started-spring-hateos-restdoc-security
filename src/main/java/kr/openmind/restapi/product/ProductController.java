6475f12fcaf0022bf7e7b8af4b7ec0865f937424
openmind-kr <slanderer@hanmail.net>
2018-11-28 01:01:42 +0900

    Polish logging - custom annotation

--- src/main/java/kr/openmind/restapi/product/ProductController.java
+++ src/main/java/kr/openmind/restapi/product/ProductController.java
@@ -1,6 +1,7 @@
 package kr.openmind.restapi.product;
 
 import kr.openmind.restapi.account.Account;
+import kr.openmind.restapi.account.CurrentAccount;
 import kr.openmind.restapi.common.ErrorResource;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -11,7 +12,6 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,28 +38,25 @@ public class ProductController {
     private static final ProductController CONTROLLER_LINK = methodOn(ProductController.class);
 
     @GetMapping
-    public ResponseEntity list(Pageable pageable,
-                               PagedResourcesAssembler<Product> assembler,
-                               @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : account") Account user) {
-        if (user == null) {
-            log.debug("allowed user doesn't exist");
+    public ResponseEntity list(Pageable pageable, PagedResourcesAssembler<Product> assembler, @CurrentAccount Account account) {
+        if (account == null) {
+            log.debug("allowed account doesn't exist");
         } else {
-            log.debug("allowed user. id: {}, email: {}, role: {}", user.getId(), user.getEmail(), user.getRoles());
+            log.debug("allowed account. id: {}, email: {}, role: {}", account.getId(), account.getEmail(), account.getRoles());
         }
 
         Page<Product> products = productRepository.findAll(pageable);
 
         PagedResources<ProductResource> productResources = assembler.toResource(products, entity -> new ProductResource(entity));
-        productResources.add(linkTo(CONTROLLER_LINK.get(null, user)).withRel("product"));
-        productResources.add(linkTo(CONTROLLER_LINK.create(null, null, user)).withRel("product-create"));
+        productResources.add(linkTo(CONTROLLER_LINK.get(null, account)).withRel("product"));
+        productResources.add(linkTo(CONTROLLER_LINK.create(null, null, account)).withRel("product-create"));
         productResources.add(createProfileLink("resources-product-get-list"));
 
         return ResponseEntity.ok(productResources);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity get(@PathVariable Integer id,
-                              @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : account") Account user) {
+    public ResponseEntity get(@PathVariable Integer id, @CurrentAccount Account account) {
         Optional<Product> optionalProduct = productRepository.findById(id);
         if (!optionalProduct.isPresent()) {
             return ResponseEntity.notFound().build();
@@ -68,15 +65,15 @@ public class ProductController {
         Product product = optionalProduct.get();
 
         ProductResource productResource = new ProductResource(product, createProfileLink("resources-product-get"));
-        productResource.add(linkTo(CONTROLLER_LINK.list(null, null, user)).withRel("products"));
-        productResource.add(linkTo(CONTROLLER_LINK.update(product.getId(), null, null, user)).withRel("update"));
+        productResource.add(linkTo(CONTROLLER_LINK.list(null, null, account)).withRel("products"));
+        productResource.add(linkTo(CONTROLLER_LINK.update(product.getId(), null, null, account)).withRel("update"));
 
         return ResponseEntity.ok(productResource);
     }
 
     @PostMapping
     public ResponseEntity create(@RequestBody @Valid ProductRequestDto productRequestDto, Errors errors,
-                                 @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : account") Account user) {
+                                 @CurrentAccount Account account) {
         if (errors.hasErrors()) {
             return ResponseEntity.badRequest().body(new ErrorResource(errors));
         }
@@ -86,8 +83,8 @@ public class ProductController {
 
         URI location = linkTo(ProductController.class).slash(savedProduct.getId()).toUri();
         ProductResource productResource = new ProductResource(savedProduct, createProfileLink("resources-product-post"));
-        productResource.add(linkTo(CONTROLLER_LINK.get(savedProduct.getId(), user)).withRel("product"));
-        productResource.add(linkTo(CONTROLLER_LINK.update(savedProduct.getId(), null, null, user)).withRel("update"));
+        productResource.add(linkTo(CONTROLLER_LINK.get(savedProduct.getId(), account)).withRel("product"));
+        productResource.add(linkTo(CONTROLLER_LINK.update(savedProduct.getId(), null, null, account)).withRel("update"));
 
         return ResponseEntity.created(location).body(productResource);
     }
@@ -95,7 +92,7 @@ public class ProductController {
     @PutMapping("/{id}")
     public ResponseEntity update(@PathVariable Integer id,
                                  @RequestBody @Valid ProductRequestDto productRequestDto, Errors errors,
-                                 @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : account") Account user) {
+                                 @CurrentAccount Account account) {
         if (errors.hasErrors()) {
             return ResponseEntity.badRequest().body(new ErrorResource(errors));
         }
@@ -109,8 +106,8 @@ public class ProductController {
         modelMapper.map(productRequestDto, product);
 
         ProductResource productResource = new ProductResource(product, createProfileLink("resources-product-put"));
-        productResource.add(linkTo(CONTROLLER_LINK.list(null, null, user)).withRel("products"));
-        productResource.add(linkTo(CONTROLLER_LINK.get(product.getId(), user)).withRel("product"));
+        productResource.add(linkTo(CONTROLLER_LINK.list(null, null, account)).withRel("products"));
+        productResource.add(linkTo(CONTROLLER_LINK.get(product.getId(), account)).withRel("product"));
 
         return ResponseEntity.ok(productResource);
     }
