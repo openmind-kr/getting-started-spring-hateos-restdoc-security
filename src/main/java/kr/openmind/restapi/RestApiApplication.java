c5676139ea1a639a5b73d1c47ce0ea6316d5b462
openmind-kr <slanderer@hanmail.net>
2018-11-27 17:24:12 +0900

    Polish - application config

--- src/main/java/kr/openmind/restapi/RestApiApplication.java
+++ src/main/java/kr/openmind/restapi/RestApiApplication.java
@@ -1,17 +1,10 @@
 package kr.openmind.restapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApiApplication {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestApiApplication.class, args);
